5fd5f1122f0b87818c7ed10ab11432a7b25b5631
lidongchao <1148238139@qq.com>
2024-05-14 23:42:30 +0800

    shadcn登录界面初步完成

--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,21 +1,32 @@
 import { useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { message } from 'antd';
+import { message, Image } from 'antd';
 import { z } from "zod";
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 
+import loginImg from "@/assets/loginImg.jpg"
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+import { Card } from '@/components/ui/card';
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
 import { useAppDispatch, useAppSelector } from '@/hooks';
 import fetchLogin from '@/store/modules/fetchLogin';
 
 
-const LoginSchema = z.object({
-    username: z.string().min(
+const loginSchema = z.object({
+    username: z.string().min(1, {message: "请填写用户名"}).min(
         2, { message: "用户名长度不能小于2个字符" }).max(
             50, { message: "用户名长度不能大于50个字符" }
         ),
-    password: z.string().min(
+    password: z.string().min(1, {message: "请填写密码"}).min(
         6, { message: "密码长度不能小于6个字符" }).max(
             50, { message: "密码长度不能大于50个字符" }
         )
@@ -39,79 +50,106 @@ export default function Login() {
         }
     }, [token, loginError, navigate])
 
-    const clickLogin = async (values: any) => {
-        console.log(values);
+    const form = useForm<z.infer<typeof loginSchema>>({
+        resolver: zodResolver(loginSchema),
+        defaultValues: {
+            username: "",
+            password: ""
+        },
+    })
 
+    const onSubmit = async (values: z.infer<typeof loginSchema>) => {
         await dispatch(fetchLogin(values))
     };
 
 
     return (
-        <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[800px]">
-            <div className="hidden bg-muted lg:block">
-                {/* TODO: 此处应当添加一张图片 */}
-                {/* <Image
-                    src="/placeholder.svg"
-                    alt="Image"
-                    width="1920"
-                    height="1080"
-                    className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-                /> */}
-            </div>
-            <div className="flex items-center justify-center py-12">
-                <div className="mx-auto grid w-[350px] gap-6">
-                    <div className="grid gap-2 text-center">
-                        <h1 className="text-3xl font-bold">LLM-PLUS</h1>
-                        <p className="text-balance text-muted-foreground">
-                            欢迎使用 LLM-PLUS
-                        </p>
-                    </div>
-                    <div className="grid gap-4">
-                        <div className="grid gap-2">
-                            <Label htmlFor="username">用户名</Label>
-                            <Input
-                                id="username"
-                                type="username"
-                                placeholder="请输入您的用户名"
-                                required
-                            />
-                        </div>
-                        <div className="grid gap-2">
-                            <div className="flex items-center">
-                                <Label htmlFor="password">密码</Label>
-                                {/* TODO: 忘记密码尚未实现 */}
-                                <Link
-                                    to="/forgot-password"
-                                    className="ml-auto inline-block text-sm underline"
-                                >
-                                    忘记密码
-                                </Link>
-                            </div>
-                            <Input id="password" type="password" placeholder="请输入您的密码" required />
-                        </div>
-                        <Button
-                            type="submit"
-                            className="w-full"
-                            onClick={clickLogin}
-                        >
-                            立即登录
-                        </Button>
-                        {/* TODO: 手机号登录尚未实现 */}
-                        <Button
-                            variant="outline"
-                            className="w-full">
-                            手机号登陆
-                        </Button>
-                    </div>
-                    <div className="mt-4 text-center text-sm">
-                        没有账号？{" "}
-                        {/* TODO: 立即注册尚未实现 */}
-                        <Link to="#" className="underline">
-                            立即注册
-                        </Link>
-                    </div>
+        <div className="w-full h-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-[700px]">
+            <div className="hidden h-full w-full lg:block bg-white">
+                <div className='flex items-center justify-center h-full w-full'>
+                    <Image
+                        src={loginImg}
+                        width={700}
+                        preview={false}
+                    />
                 </div>
             </div>
-        </div>
+            <div className="flex items-center justify-center py-12">
+
+                <Card className='w-[400px] h-[500px]'>
+                    <div className="mx-auto grid w-[350px] gap-6">
+                        <div className="grid gap-2 text-center">
+                            <h1 className="text-3xl font-bold">LLM-PLUS</h1>
+                            <p className="text-balance text-muted-foreground">
+                                欢迎使用 LLM-PLUS
+                            </p>
+                        </div>
+                        <div className="grid gap-4 w-full h-full">
+                            <Form {...form}>
+                                <form onSubmit={form.handleSubmit(onSubmit)}>
+                                    <div className='grid gap-2'>
+                                        <FormField
+                                            control={form.control}
+                                            name="username"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <FormLabel>用户名</FormLabel>
+                                                    <FormControl>
+                                                        <Input placeholder="请输入用户名" {...field} />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </div>
+                                    <div className='grid gap-2 mt-2'>
+                                        <FormField
+                                            control={form.control}
+                                            name="password"
+                                            render={({ field }) => (
+                                                <FormItem>
+                                                    <div className='flex items-center'>
+                                                        <FormLabel>
+                                                            密码
+                                                        </FormLabel>
+                                                        {/* TODO: 忘记密码尚未实现 */}
+                                                        <Link
+                                                            to="#"
+                                                            className="ml-auto inline-block text-sm underline"
+                                                        >
+                                                            忘记密码
+                                                        </Link>
+                                                    </div>
+                                                    <FormControl>
+                                                        <Input type='password' placeholder="请输入密码" {...field} />
+                                                    </FormControl>
+                                                    <FormMessage />
+                                                </FormItem>
+                                            )}
+                                        />
+                                    </div>
+                                    <Button type="submit" className='w-full mt-4'>
+                                        立即登录
+                                    </Button>
+                                </form>
+                            </Form>
+                            {/* TODO: 手机号登录尚未实现 */}
+                            <Button
+                                variant="outline"
+                                className="w-full">
+                                手机号登陆
+                            </Button>
+                        </div>
+                        <div className="mt-4 text-center text-sm">
+                            没有账号？{" "}
+                            {/* TODO: 立即注册尚未实现 */}
+                            <Link to="#" className="underline">
+                                立即注册
+                            </Link>
+                        </div>
+                    </div>
+                </Card>
+            </div>
+        </div >
     )
 }
